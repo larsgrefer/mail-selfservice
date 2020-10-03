be95e668f2f2aabd02fc5bca66003f02d9063cae
Lars Grefer <github@larsgrefer.de>
2020-10-03 02:42:37 +0200

    Migrate to JUnit Jupiter

--- src/test/java/de/greferhosting/selfservice/SelfServiceApplicationTests.java
+++ src/test/java/de/greferhosting/selfservice/SelfServiceApplicationTests.java
@@ -1,15 +1,13 @@
 package de.greferhosting.selfservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SelfServiceApplicationTests {
 
